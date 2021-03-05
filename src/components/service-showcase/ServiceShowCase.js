885ca737de143a149ff60faee94dcedeecc91123
intansemc2 <intansemc2@gmail.com>
2021-03-05 18:58:13 +0700

    Thêm Icon

--- src/components/service-showcase/ServiceShowCase.js
+++ src/components/service-showcase/ServiceShowCase.js
@@ -3,9 +3,9 @@ import React from 'react';
 function ServiceShowCase() {
     return (
         <div className="ServiceShowcase">
-            <div class="container">
-                <div class="row mb-4 align-items-center">
-                    <div class="col-md-6" data-aos="fade-up">
+            <div className="container">
+                <div className="row mb-4 align-items-center">
+                    <div className="col-md-6" data-aos="fade-up">
                         <h2>My Services</h2>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus incidunt ut
@@ -15,14 +15,14 @@ function ServiceShowCase() {
                 </div>
             </div>
 
-            <div class="site-section pb-0">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
-                            <span class="la la-cube la-3x mb-4" />
-                            <h4 class="h4 mb-2">Web Design</h4>
+            <div className="site-section pb-0">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
+                            <span className="la la-cube la-3x mb-4" />
+                            <h4 className="h4 mb-2">Web Design</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit explicabo inventore.</p>
-                            <ul class="list-unstyled list-line">
+                            <ul className="list-unstyled list-line">
                                 <li>Lorem ipsum dolor sit amet consectetur adipisicing</li>
                                 <li>Non pariatur nisi</li>
                                 <li>Magnam soluta quod</li>
@@ -30,11 +30,11 @@ function ServiceShowCase() {
                                 <li>Cumque quae aliquam</li>
                             </ul>
                         </div>
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="100">
-                            <span class="la la-mobile la-3x mb-4" />
-                            <h4 class="h4 mb-2">Mobile Applications</h4>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="100">
+                            <span className="la la-mobile la-3x mb-4" />
+                            <h4 className="h4 mb-2">Mobile Applications</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit explicabo inventore.</p>
-                            <ul class="list-unstyled list-line">
+                            <ul className="list-unstyled list-line">
                                 <li>Lorem ipsum dolor sit amet consectetur adipisicing</li>
                                 <li>Non pariatur nisi</li>
                                 <li>Magnam soluta quod</li>
@@ -42,11 +42,11 @@ function ServiceShowCase() {
                                 <li>Cumque quae aliquam</li>
                             </ul>
                         </div>
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
-                            <span class="la la-image la-3x mb-4" />
-                            <h4 class="h4 mb-2">Graphic Design</h4>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
+                            <span className="la la-image la-3x mb-4" />
+                            <h4 className="h4 mb-2">Graphic Design</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit explicabo inventore.</p>
-                            <ul class="list-unstyled list-line">
+                            <ul className="list-unstyled list-line">
                                 <li>Lorem ipsum dolor sit amet consectetur adipisicing</li>
                                 <li>Non pariatur nisi</li>
                                 <li>Magnam soluta quod</li>
@@ -54,11 +54,11 @@ function ServiceShowCase() {
                                 <li>Cumque quae aliquam</li>
                             </ul>
                         </div>
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="100">
-                            <span class="la la-search la-3x mb-4" />
-                            <h4 class="h4 mb-2">SEO</h4>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="100">
+                            <span className="la la-search la-3x mb-4" />
+                            <h4 className="h4 mb-2">SEO</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit explicabo inventore.</p>
-                            <ul class="list-unstyled list-line">
+                            <ul className="list-unstyled list-line">
                                 <li>Lorem ipsum dolor sit amet consectetur adipisicing</li>
                                 <li>Non pariatur nisi</li>
                                 <li>Magnam soluta quod</li>
